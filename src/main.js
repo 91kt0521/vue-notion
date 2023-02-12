17a1021ef5b85e4053376561ce0ba5aa792abeb5
K_T <armn7guc.supbs.3@gmail.com>
2023-02-12 21:35:49 +0900

    add:VueRouter読み込み

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// VueRouterを使用
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+const router = new VueRouter({
+    mode:'history',
+    routes:[
+
+    ]
+})
+
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
