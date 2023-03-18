a6fea71fbd71c2359f62ed40fcff2f9fb09300fc
K_T <armn7guc.supbs.3@gmail.com>
2023-03-18 23:18:26 +0900

    update : ディレクティブ、イベント処理学習追加

--- src/components/study/DirectivePage.vue
+++ src/components/study/DirectivePage.vue
@@ -1,11 +1,43 @@
 <template>
   <div class="directive-page">
     <h2>学習2：ディレクティブ</h2>
+
+    <!-- 従来のjavascript -->
+    <h3><span class="badge badge-secondary">Javascript</span></h3>
+    <input id="check" type="checkbox" checked="checked">
+    <span id="text">チェックのときだけみえるよ</span>
+    <br/>
+    <br/>
+
+    <!-- Vue.js -->
+    <h3><span class="badge badge-secondary">Vue.js</span></h3>
+    <input type="checkbox" v-model="checked">
+    <span v-show="checked">チェック時だけ見えるよ</span>
   </div>
 </template>
 
 <script>
+// 従来のjavascript
+window.onload = function() {
+    const checkbox = document.getElementById('check');
+    checkbox.onchange = function(event) {
+        const checked = event.target.checked;
+        const text = document.getElementById('text');
+        if (checked) {
+        text.style.visibility = 'visible';
+        } else {
+        text.style.visibility = 'hidden';
+        }
+    };
+}
+
+
 export default {
+    data() {
+        return {
+            checked : true
+        }
+    }
 }
 </script>
 
